653d3b9a98745bf48e75ec7659f072b82d7f3516
Mohammed Al zobie <34588496+mohammed-safaa@users.noreply.github.com>
2020-01-20 14:25:15 +0300

    Create app-img-api.js

---
+++ app-img-api.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+
+export default class FetchExample extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('http://192.168.1.104/API-in-PHP/read.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.name_movies}, {item.about},{item.category}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
